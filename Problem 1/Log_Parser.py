37a50465717083e48415f855154f6dce5537379b
rafalslaby <rafals222@outlook.com>
2017-04-05 12:45:37 +0200

    Adding visualization

--- Problem 1/Log_Parser.py
+++ Problem 1/Log_Parser.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import matplotlib.pyplot as plt
 
 class LogParser:
     def IsValid(self, file):
@@ -17,10 +18,30 @@ class LogParser:
                         List.append(linescounter)
                     linescounter+=1
                 print('string: "',string_to_be_found, '" was found ',counter,' times in the following lines:\n', List, sep='')
+
+                #plotting part, search for SUCCESS
+                for i in range(0, len(List)):
+                    List[i] //= 100
+                print(List)
+                PlotList=[]
+                for i in range(0, linescounter//100):
+                    counter=0
+                    for j in List:
+                        if j == i:
+                            counter+=1
+                    PlotList.append(counter)
+
+                plt.plot(PlotList)
+                plt.title(string_to_be_found + ' in ' + input_file)
+                plt.ylabel(string_to_be_found + 's within x hundred line of file')
+                plt.xlabel('x-hundred line in file')
+                plt.savefig('plot.png')
+
         else:
             print('Theres no such file :',input_file )
 
 if __name__=='__main__':
     with open('log_parser_output.txt', 'w') as sys.stdout:
         parser = LogParser()
-        parser.parse_log(sys.argv[1],sys.argv[2])
+        parser.parse_log(sys.argv[1], sys.argv[2])
+
