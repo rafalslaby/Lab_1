84cbf4510af914953ad4112f72e45c2c940eefad
rafalslaby <rafals222@outlook.com>
2017-04-05 10:31:14 +0200

    adding input validation

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -1,15 +1,20 @@
 from AbstractCalculator import AbstractCalculator
-
+from Validator import Validator
 
 class Calculator(AbstractCalculator):
     def add(self, arg1, arg2):
+        Validator.validate(arg1,arg2)
         return arg1+arg2
 
     def subtract(self, arg1, arg2):
+        Validator.validate(arg1, arg2)
         return arg1-arg2
 
     def divide(self,arg1, arg2):
+        Validator.validate(arg1, arg2)
+        Validator.division_by_zero(arg2)
         return arg1/arg2
 
     def derivative(self, function, n):
         return '2x'
+
